4a854643eafc37f5e62fa053cc9b67f90f727102
Babin Nikolay <babin.nik@gmail.com>
2025-10-28 02:07:41 +0300

    fix clients test

--- app/_components/Clients.js
+++ app/_components/Clients.js
@@ -1,19 +1,45 @@
-import React, { Suspense } from "react";
+import React from "react";
 import SectionLine from "@/app/_components/SectionLine";
 import SectionLabel from "@/app/_components/SectionLabel";
-import ClientsList from "@/app/_components/ClientsList";
 import NewItemButton from "@/app/_components/NewItemButton";
+import TableHeader from "@/app/_components/TableHeader";
+import TableRow from "@/app/_components/TableRow";
+import ListButtons from "@/app/_components/ListButtons";
+import Table from "@/app/_components/Table";
 
-export default function Clients() {
+export default async function Clients() {
+  const { PrismaClient } = await import("@prisma/client");
+  const prisma = new PrismaClient();
+  let clients = [];
+  try {
+    clients = await prisma.client.findMany();
+  } catch (err) {
+    console.error(err);
+  }
+
   return (
     <div>
       <SectionLine>
         <SectionLabel>Clients</SectionLabel>
         <NewItemButton href={"/clients/new"} title={"New client"} />
       </SectionLine>
-      <Suspense fallback={<div>Loading clients...</div>}>
-        <ClientsList />
-      </Suspense>
+
+      <Table>
+        <TableHeader>
+          <div className="flex-1">Client name</div>
+          <div className="flex-none w-32 text-center">Actions</div>
+        </TableHeader>
+        {clients.map((client, index) => (
+          <TableRow key={index}>
+            <div className="flex-1">{client.name}</div>
+            <ListButtons href={`/clients/edit/${client.id}`} />
+          </TableRow>
+        ))}
+      </Table>
+
+      {/*<Suspense fallback={<div>Loading clients...</div>}>*/}
+      {/*  <ClientsList />*/}
+      {/*</Suspense>*/}
     </div>
   );
 }

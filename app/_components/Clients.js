edf73e444813b378e67e23e7a4263024ab38d76f
Babin Nikolay <babin.nik@gmail.com>
2025-10-28 01:45:18 +0300

    fix deploy

--- app/_components/Clients.js
+++ app/_components/Clients.js
@@ -1,17 +1,19 @@
-import React from "react";
+import React, { Suspense } from "react";
 import SectionLine from "@/app/_components/SectionLine";
 import SectionLabel from "@/app/_components/SectionLabel";
 import ClientsList from "@/app/_components/ClientsList";
 import NewItemButton from "@/app/_components/NewItemButton";
 
-export default function Clients({ children }) {
+export default function Clients() {
   return (
     <div>
       <SectionLine>
         <SectionLabel>Clients</SectionLabel>
         <NewItemButton href={"/clients/new"} title={"New client"} />
       </SectionLine>
-      <ClientsList />
+      <Suspense fallback={<div>Loading clients...</div>}>
+        <ClientsList />
+      </Suspense>
     </div>
   );
 }

d0d5a79c9417615ea67f2afc806df99d9e6b95e8
Babin Nikolay <babin.nik@gmail.com>
2025-11-02 18:49:12 +0300

    fix dashboard

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
 import * as appConstants from "@/app/_helpers/appConstants";
 import Dashboard from "@/app/_components/dashboard/Dashboard";
 
+export const revalidate = 0;
+
 export async function generateMetadata() {
   return {
     title: `Home ${appConstants.TITLE_SEPARATOR} ${appConstants.APP_NAME}`,
@@ -11,11 +13,6 @@ export default function Home() {
   return (
     <main>
       <Dashboard />
-      {/*<div className="relative z-10 text-center">*/}
-      {/*  <h1 className="text-8xl text-primary-50 mb-10 tracking-tight font-normal">*/}
-      {/*    Welcome to {appConstants.APP_NAME}.*/}
-      {/*  </h1>*/}
-      {/*</div>*/}
     </main>
   );
 }

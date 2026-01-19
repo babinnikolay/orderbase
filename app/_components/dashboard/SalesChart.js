5098ae010817a23beb5aff51d114292aab568f34
Babin Nikolay <babin.nik@gmail.com>
2026-01-19 14:43:38 +0300

    new sales chart

--- app/_components/dashboard/SalesChart.js
+++ app/_components/dashboard/SalesChart.js
@@ -1,13 +1,6 @@
 "use client";
 import React from "react";
-import {
-  CartesianGrid,
-  Line,
-  LineChart,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from "recharts";
+import { Bar, BarChart, CartesianGrid, Tooltip } from "recharts";
 
 function SalesChart({ chartData }) {
   return (
@@ -15,19 +8,18 @@ function SalesChart({ chartData }) {
       <p className="pl-6 pb-2 text-2xl">
         Sales and payments for the last 30 days
       </p>
-      <LineChart width="100%" height={300} data={chartData} responsive>
+      <BarChart width="100%" height={300} data={chartData} responsive>
         <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey="date" />
-        <YAxis />
-        <Tooltip />
-        <Line type="monotone" dataKey="sales" stroke="#8884d8" name="sales" />
-        <Line
+
+        <Tooltip cursor={false} />
+        <Bar type="monotone" dataKey="sales" name="sales" fill="#5E82A6" />
+        <Bar
           type="monotone"
           dataKey="payments"
-          stroke="#82ca9d"
+          fill="#82ca9d"
           name="payments"
         />
-      </LineChart>
+      </BarChart>
     </div>
   );
 }
